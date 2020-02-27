0d48cac37d8d2ad3adc4b243d5705c342677304b
alexey <alexeyorc@gmail.com>
2020-02-28 01:33:26 +0200

    Added GA actions

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Data: https://sites.google.com/site/imediatreat/data-sets?fbclid=IwAR3s-YyUvktVRqPFRXNKRbnk9ulMCSlV0YAqvjxHNYwbU82qWRRJoYRJtHo
+
+Explanation of GA: https://towardsdatascience.com/introduction-to-genetic-algorithms-including-example-code-e396e98d8bf3
+
+Explanation of PyTorch: https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html
--- deep.py
+++ deep.py
@@ -1,7 +1,11 @@
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.nn as nn
 import torch
 import torchvision
 import torchvision.transforms as transforms
 import numpy as np
+import random
 
 # numpy train test split NFoldCrossValidation
 transform = transforms.Compose(
@@ -11,6 +15,7 @@ transform = transforms.Compose(
 trainset = torchvision.datasets.ImageFolder(
     root="./data", transform=transform
 )
+
 trainloader = torch.utils.data.DataLoader(
     trainset, batch_size=10, shuffle=True, num_workers=2
 )
@@ -18,10 +23,12 @@ trainloader = torch.utils.data.DataLoader(
 testset = torchvision.datasets.ImageFolder(
     root="./test", transform=transform
 )
+
 testloader = torch.utils.data.DataLoader(
     testset, batch_size=10, shuffle=False, num_workers=2
 )
 
+# Classes, G0 stands for normal diagnosis.
 classes = (
     "G0",
     "G1",
@@ -29,11 +36,13 @@ classes = (
     "G3",
 )
 
-import torch.nn as nn
-import torch.nn.functional as F
-
 
 class Net(nn.Module):
+    """
+    Classic PyTorch CNN model for image classification.
+    See: https://pytorch.org/tutorials/
+    """
+
     def __init__(self):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(3, 6, 5)
@@ -52,21 +61,54 @@ class Net(nn.Module):
         x = self.fc3(x)
         return x
 
+
 net = Net()
 
-import torch.optim as optim
+# Initial population size for every epoch
+POP_SIZE = 10
+
+# Always odd value!
+DEFAULT_ELITE_SIZE = 6
+
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-def gen_population(shape, size=10):
+
+def gen_population(shape, size=POP_SIZE):
     res = []
     for i in range(size):
-        #WEIHTS NORMALIZATION
-        # intree ca -1 si 1
-        # see net.conv1.weight
         res.append(torch.randn(shape))
     return res
+
+
+def get_fittest(list_of_cand, elite_size=DEFAULT_ELITE_SIZE):
+    """
+    List of caditates is always sorted by first value of
+    tuple (loss value) in ASC order
+    """
+    selected = []
+    for i in range(elite_size):
+        selected.append(list_of_cand[i][1])
+
+    selection_size = int(random.randrange(len(list_of_cand) + 1))
+
+    if selection_size % 2 == 1:
+        selection_size -= 1
+
+    for i in range(elite_size, selection_size):
+        selected.append(list_of_cand[i][1])
+
+    return selected
+
+
+def cross_by_two(p1, p2):
+    pass
+
+
+def do_crossover(chromozomes):
+    pass
+
 
 # Salvez acuratetea si lossul pe testing dupa ce iau cel rezultat
 # Amestecand datele
@@ -74,61 +116,48 @@ def gen_population(shape, size=10):
 # losst
 # Use normalized loss -1 ---- 1 to mutate random weigth using mutation algo.
 def run():
-    # Shuffle dataset 
+    # Shuffle dataset
     for epoch in range(1):  # loop over the dataset multiple times
+        with torch.no_grad():
+            # print(net.conv1.weight)
+            # print(net.fc1.weight)
+            # print(net.fc2.weight.shape)
+            # print(net.fc3.weight.shape)
 
-        print(net.conv1.weight.shape)
-        print(net.conv2.weight.shape)
-        print(net.fc1.weight.shape)
-        print(net.fc2.weight.shape)
-        print(net.fc3.weight.shape)
+            # t = net.conv1.weight
+            # t = t.view(np.prod(t.shape))
+            # print(t)
+            # t = t.view(net.conv1.weight.shape)
+            # print(t)
 
-        t = net.conv1.weight
-        t = t.view(np.prod(t.shape))
-        print(t)
-        t = t.view(net.conv1.weight.shape)
-        print(t)
+            conv1_pop = gen_population(net.conv1.weight.shape)
+            # print(conv1_pop)
 
-        conv1_pop = gen_population(net.conv1.weight.shape)
-        conv2_pop = gen_population(net.conv2.weight.shape)
+            # run other data generated population. adjust weights, select the best boi in every epoch
+            # add the best boio to final pop
+            # select from final pop boio with best fitness
+            # check result
+            for i, data in enumerate(trainloader, 0):
+                # get the inputs; data is a list of [inputs, labels]
+                inputs, labels = data
 
-        # run other data generated population. adjust weights, select the best boi in every epoch
-        # add the best boio to final pop
-        # select from final pop boio with best fitness
-        # check result
-        running_loss = 0.0
-        for i, data in enumerate(trainloader, 0):
-            # get the inputs; data is a list of [inputs, labels]
-            inputs, labels = data
+                cv1_fit = []
+                # Get fitness for gen
+                for i in range(POP_SIZE):
 
-            cv1_fit = {}
-            cv2_fit = {}
-            # Get fitness for gen
-            for i in range(10):
-                with torch.no_grad():
                     net.conv1.weight = torch.nn.Parameter(conv1_pop[i])
-                    net.conv2.weight = torch.nn.Parameter(conv2_pop[i])
-                
-                outputs = net(inputs)
-                loss = criterion(outputs, labels)
-                # print(loss)
-                cv1_fit[loss.item()] = conv1_pop[i]
-                cv2_fit[loss.item()] = conv2_pop[i]
 
-            # cv1_fit = sorted(cv1_fit)
-            print(cv1_fit)
+                    outputs = net(inputs)
+                    loss = criterion(outputs, labels)
+                    # print(loss)
+                    cv1_fit.append((loss.item(), conv1_pop[i].view(
+                        np.prod(net.conv1.weight.shape))))
 
-            # loss.backward()
-            # optimizer.step()
-
-            # running_loss += loss.item()
-            # print('[%d, %5d] loss: %.3f' %
-            #     (epoch + 1, i + 1, running_loss))
-            # running_loss = 0.0
+                cv1_fit = sorted(cv1_fit, key=lambda x: x[0])
+                cv1_selected = get_fittest(cv1_fit)
+                print(cv1_selected)
     print('Finished Training')
-    
+
 
 if __name__ == '__main__':
-    # pass
     run()
-#https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html
